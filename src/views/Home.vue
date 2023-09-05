4eecf58b6ce3862d758f9730066492ec595be51e
Henrique Duarte <henrique@tiselect.com.br>
2023-09-05 14:20:03 -0300

    configuring route;

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { onMounted } from 'vue'
+  import ListMovies from '@/components/ListMovies/Index.vue'
+  import { useMovieStore } from '@/stores/movie'
+  import MovieService from '@/services/movie'
+
+  const store = useMovieStore()
+
+  onMounted(async () => {
+    store.setMovie(await MovieService.getListMovie())
+    console.log(store.getMovie)
+  })
+
+</script>
+
+<template>
+  <div>
+    <ListMovies />
+  </div>
+</template>
