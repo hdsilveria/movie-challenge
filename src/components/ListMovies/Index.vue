93ecd3662bf37008e9ece36b5475bd5f84381584
Henrique Duarte <henrique@tiselect.com.br>
2023-09-05 14:22:00 -0300

    created core components;

---
+++ src/components/ListMovies/Index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { useMovieStore } from '@/stores/movie'
+    const store = useMovieStore()
+</script>
+
+<template>
+    <div class="list-movies">
+        <h1 class="list-movies__title">Teste tecnico de listagem de filmes</h1>
+        
+        <section class="list-movies__content">
+
+        </section>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
